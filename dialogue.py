5e973349cc94f35a5f5a3cecabd1889596552aa1
Derek Bohman <derekbohman@Dereks-MBP.hsd1.or.comcast.net>
2022-03-26 20:20:05 -0700

    Created a dialogue module for the text from the story, and started writing content.

---
+++ dialogue.py
@@ -0,0 +1,23 @@
+def dialogue_one():
+    print('''
+
+    Our story begins with our hero, Hercules, as he was training with Chiron the centaur.)
+
+"Hercules! I told you, you have have to keep your shield arm up while attacking!"
+
+"Chiron, no one is able to hurt me. Why do I have to even have to bother with this shield?")
+
+"You'll change your opinion if you are struck with an arrow that's been coated with the blood of Nessus because you
+
+didn't keep your shield arm up."
+
+"I think I'll take my chances, Chiron."''')
+
+def dialogue_two():
+    print('''
+    
+    At that moment, Hermes suddenly appeared in a flash of light.
+
+"Hermes. What brings you here, my nimble friend?"
+
+''')
